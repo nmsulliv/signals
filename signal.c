43188f6018ee9fe01e5c6c4ffb57a7393c35be90
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-09-23 18:11:23 +0000

    Move signal outside the loop

--- signal.c
+++ signal.c
@@ -10,14 +10,14 @@ int g = 0;
 void handler(int signum) { //signal handler
   printf("Hello World!\n");
   g = 1;
+	alarm(1); //Schedule a SIGALRM for 1 second
 }
 
 int main(int argc, char * argv[]) {
   signal(SIGALRM, handler); //register handler to handle SIGALRM
-  
-  while(1) {
-    alarm(1); //Schedule a SIGALRM for 1 second
+  alarm(1); //Schedule a SIGALRM for 1 second
 
+  while(1) {
     while(g == 0); //busy wait for signal to be delivered
     printf("Turing was right!\n");
     g = 0;
