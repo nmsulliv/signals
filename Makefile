249bfd5ab60c381b9de87c56e1d905d2e168a6c4
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-09-23 15:19:21 +0000

    Create separate directory for timer and timer makefile

--- Makefile
+++ Makefile
@@ -1,5 +1,3 @@
 signals: signal.c
 	gcc signal.c -o signalab
 
-timer: timer.c
-	gcc timer.c -o timer 
---
+++ timer/timer.c
@@ -0,0 +1,10 @@
+/* timer.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char * argv[]) {
+  return 0;
+}
+
+
