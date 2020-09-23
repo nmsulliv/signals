600a55764e4829ac6b59b1fde0b46fe94aed0d22
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-09-23 17:53:17 +0000

    Simplify loop

--- timer/timer.c
+++ timer/timer.c
@@ -3,33 +3,29 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <time.h>
 
 /* global variable */
 int count = 0;
-int g = 0;
 
-void counter(int signum) { //signal handler
+void counter(int signum) {
   count++;
 	printf("Count: %d\n", count);
-	g = 1;
+	alarm(1); //Schedule a SIGALRM for 1 second
 }
 
-void handler(int signum) {
+void stop(int signum) { //signal handler
 	printf("\nRan for %d seconds.\n", count);
 	exit(0);
 }
 
 int main(int argc, char * argv[]) {
   signal(SIGALRM, counter); //register handler to handle SIGALRM
-  
-  while(1) {
-    alarm(1); //Schedule a SIGALRM for 1 second
+  signal(SIGINT, stop); //register handler to handle SIGINT
 
-    while(g == 0); //busy wait for signal to be delivered
-    g = 0;
-		
-		signal(SIGINT, handler); //register handler to handle SIGINT
-  }
+	alarm(1); //Schedule a SIGALRM for 1 second
+
+  while(1);
 
   return 0; //never reached
 }
