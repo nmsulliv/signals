531456d851b37a8a04a10aa83b971e7dde2dc7a8
Nicole Sullivan <nicolesullivanmarie@gmail.com>
2020-09-23 15:07:49 +0000

    Print out two statements every 1 second infinetly

--- signal.c
+++ signal.c
@@ -4,16 +4,26 @@
 #include <signal.h>
 #include <unistd.h>
 
-void handler(int signum)
-{ //signal handler
+/* global variable */
+int g = 0;
+
+void handler(int signum) { //signal handler
   printf("Hello World!\n");
-  exit(1); //exit after printing
+  g = 1;
 }
 
-int main(int argc, char * argv[])
-{
-  signal(SIGALRM,handler); //register handler to handle SIGALRM
-  alarm(1); //Schedule a SIGALRM for 1 second
-  while(1); //busy wait for signal to be delivered
+int main(int argc, char * argv[]) {
+  signal(SIGALRM, handler); //register handler to handle SIGALRM
+  
+  while(1) {
+    alarm(1); //Schedule a SIGALRM for 1 second
+
+    while(g == 0); //busy wait for signal to be delivered
+    printf("Turing was right!\n");
+    g = 0;
+  }
+
   return 0; //never reached
 }
+
+
